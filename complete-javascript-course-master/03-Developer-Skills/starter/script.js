6da09ad28bd197dbe51de8e5d705c524d9854ac3
M SHREYASKUMAR SHARMA <93853805+24Shreyaskumar@users.noreply.github.com>
2025-11-07 11:14:32 +0530

    JS - section 3 : Dev skills and editor setup

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// trying out prettier
+const x = 23;
+
+// trying out code snippets
+console.log("typed cl to check console.log");
+
+// Debugging techniques -
+// 1. console.log()
+// 2. debugger keyword
+// 3. breakpoints in browser dev tools
